19c2911ccb7e93e80391517fea7914150104be82
hirocueki <hirocueki@gmail.com>
2019-10-10 16:29:43 +0900

    product スペックの修正

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it '商品の項目の確認(商品名, 商品画像, 価格（業者を超えても同一価格）,説明文, 非表示フラグ,表示順)'
+  it '商品の項目の確認(商品名, 商品画像, 価格（業者を超えても同一価格）,説明文, 非表示フラグ,表示順)' do
+    product = Product.create(
+                       name: '机',
+                       image: '',
+                       price: 6000,
+                       description: 'ありふれたデザインの机',
+                       displayed: true,
+                       order_index: 1
+    )
+    expect(product).to be_valid
+  end
 end
