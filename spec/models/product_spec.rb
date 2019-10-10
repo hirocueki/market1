318c66cc9759c0a769cae4c7ba10b1e9e8e6c030
hirocueki <hirocueki@gmail.com>
2019-10-10 16:25:29 +0900

    モデルスペック追加 user, product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  it '商品の項目の確認(商品名, 商品画像, 価格（業者を超えても同一価格）,説明文, 非表示フラグ,表示順)'
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'ユーザの項目の確認（メールアドレス, パスワード, 送付先情報（名前、住所）)' do
+    user = User.create(
+          email: 'john@example.com',
+          password: 'john12345',
+          password_confirmation: 'john12345',
+          name: 'ジョン',
+          address: '222-4444 東京都江戸川区２−３−４'
+    )
+    expect(user).to be_valid
+  end
+
+  it '名前と住所は省略できる' do
+    user = User.create(
+      email: 'john@example.com',
+      password: 'john12345',
+      password_confirmation: 'john12345',
+      )
+    expect(user).to be_valid
+  end
 end
