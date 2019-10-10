799c68c1a968dcb1d2aebdabefff7946b0e1291b
hirocueki <hirocueki@gmail.com>
2019-10-10 17:51:32 +0900

    商品のテスト追加、routes追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe "Users", type: :system do
     driven_by(:rack_test)
   end
 
-  it 'ユーザ登録できる'
+  it 'ユーザ登録できる' do
+    visit new_user_registration_path
+
+    expect {
+      fill_in 'Email', with: 'ueki@rails.com'
+      fill_in 'Password', with: 'ueki999'
+      fill_in 'Password confirmation', with: 'ueki999'
+      click_on 'Sign up'
+    }.to change{User.count}.by(1)
+
+  end
   it 'ショッピングカートを通して商品を購入できる'
 
 end
