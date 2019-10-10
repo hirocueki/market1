7a3cb56fed9fb44f334e499787d7822f9190a346
hirocueki <hirocueki@gmail.com>
2019-10-10 16:33:10 +0900

    rspec のジェネレータの設定を変更

--- config/application.rb
+++ config/application.rb
@@ -24,12 +24,16 @@ module Market1
 
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false,
+                       integration_tool: false
+    end
   end
 end
